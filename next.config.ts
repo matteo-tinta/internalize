2a67aaa357e2d120488ce07e1095fba4b40a544a
Matteo Tinta <matteo.tinta.external@ynap.com>
2024-10-30 18:32:21 +0100

    feat: added mongodb image on docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+      config.resolve.fallback.tls = false
+      config.resolve.fallback.net = false
+      config.resolve.fallback["timers/promises"] = false
+      config.resolve.fallback.dns = false
+      config.resolve.fallback.child_process = false
+    }
+
+    return config
+  }
 };
 
 export default nextConfig;
